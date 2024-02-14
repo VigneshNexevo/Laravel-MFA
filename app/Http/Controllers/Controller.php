128df34d132d31599cfc4b77d8207ce8979f94a6
Vignesh Saravanan <vignesh.nexevo@gmail.com>
2024-02-14 16:31:20 +0530

    2FA implemented

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function adduser(Request $request)
+    {
+      
+
+        $google2fa = app('pragmarx.google2fa');
+        $registration_data=[];
+        session()->put('registration_data',$registration_data);
+        $registration_data = $request->all();
+  
+        $registration_data["google2fa_secret"] = $google2fa->generateSecretKey();
+  
+        $request->session()->flash('registration_data', $registration_data);
+        session()->put('registration_data',$registration_data);
+
+        User::create([
+            'name'=>request('name'),
+            'email'=>request('email'),
+            'password'=>Hash::make('12345678'),
+            'google2fa_secret'=>$registration_data["google2fa_secret"] 
+        ]);
+  
+        $QR_Image = $google2fa->getQRCodeInline(
+            config('app.name'),
+            $registration_data['email'],
+            $registration_data['google2fa_secret']
+        );
+
+        return view('google2fa.register', ['QR_Image' => $QR_Image, 'secret' => $registration_data['google2fa_secret']]);
+    }
+
+    public function verifyuser(Request $request)
+    {
+        $data=session()->get('registration_data');
+
+        $validated = $request->validate([
+            'one_time_password' => 'required',
+        ]);
+
+        $user = User::where('email',$data['email'])->first();
+        $google2fa = app('pragmarx.google2fa');
+        dd($valid = $google2fa->verifyKey($user->google2fa_secret, $request->input('one_time_password')));
+        return request();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -22,12 +23,23 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::middleware(['2fa'])->group(function () {
+Route::post('/adduser', [Controller::class, 'adduser'])->name('user.add');
+Route::post('/2fa', [Controller::class, 'verifyuser'])->name('2fa');
+
+// Route::middleware(['2fa'])->group(function () {
    
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-    Route::post('/2fa', function () {
-        return redirect(route('home'));
-    })->name('2fa');
-});
+    // Route::post('/2fa', function () {
+    //     $validated = $request->validate([
+    //         'one_time_password' => 'required',
+    //     ]);
+
+    //     $user = Auth::user();
+    //     $google2fa = app('pragmarx.google2fa');
+    //     $valid = $google2fa->verifyKey($user->google2fa_secret, $request->input('one_time_password'));
+    //     return request();
+    //     // return redirect(route('home'));
+    // })->name('2fa');
+// });
   
 Route::get('/complete-registration', [RegisterController::class, 'completeRegistration'])->name('complete.registration');
